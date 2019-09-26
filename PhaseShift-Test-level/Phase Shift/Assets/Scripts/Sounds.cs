20fe464ba823c2567a7531b8e443451acabb9ab2
chernokm <mac_chernok@yahoo.com>
2019-09-26 16:29:31 -0400

    fixes audio and image flash

---
+++ PhaseShift-Test-level/Phase Shift/Assets/Scripts/Sounds.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Sounds : MonoBehaviour
+{
+
+    public AudioSource teleportSound;
+    public Image flash;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.F))
+        {
+            StartCoroutine(Flash());
+            teleportSound.Play();
+            //flash.enabled = true;
+            
+        }
+    }
+
+    public IEnumerator Flash()
+    {
+        flash.enabled = true;
+        yield return new WaitForSeconds(0.5f);
+        flash.enabled = false;
+    }
+}
