e91b3dd18edd05f32ab343d213dfe66c1afb81e7
chernokm <mac_chernok@yahoo.com>
2019-10-17 10:58:00 -0500

    UI elements and pick ups

--- PhaseShift-Test-level/Phase Shift/Assets/Scripts/Sounds.cs
+++ PhaseShift-Test-level/Phase Shift/Assets/Scripts/Sounds.cs
@@ -4,30 +4,25 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class Sounds : MonoBehaviour
-{
-
+{ 
+    // Defines the source of the sound and what image is shown when teleporting
     public AudioSource teleportSound;
     public Image flash;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
     void Update()
     {
+        // If the player presses the "F" key
         if(Input.GetKeyDown(KeyCode.F))
         {
+            // Starts the "Flash" Coroutine and plays the teleport sound
             StartCoroutine(Flash());
             teleportSound.Play();
-            //flash.enabled = true;
-            
         }
     }
 
     public IEnumerator Flash()
     {
+        // Enables the image "flash", waits 0.5 then disables it so teleport seems more natural
         flash.enabled = true;
         yield return new WaitForSeconds(0.5f);
         flash.enabled = false;
